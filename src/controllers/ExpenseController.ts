8498450583e2a897f69bd9dfc139b0b1c5e1a082
Pedro Brasca <pedropaulobrasca@gmail.com>
2023-11-09 23:30:02 -0300

    Refactor expense creation to use destructuring in ExpenseController.ts

--- src/controllers/ExpenseController.ts
+++ src/controllers/ExpenseController.ts
@@ -22,26 +22,15 @@ class ExpenseController {
 
   async create(req: Request, res: Response) {
     try {
-      const {
-        paid,
-        expense,
-        monthlyValue,
-        date,
-        installments,
-        totalValue,
-        description,
-        userClerkId,
-      } = req.body;
-
       const expenseData: IExpense = {
-        paid,
-        expense,
-        monthlyValue,
-        date,
-        installments,
-        totalValue,
-        description,
-        userClerkId,
+        paid: req.body.paid,
+        expense: req.body.expense,
+        monthlyValue: req.body.monthlyValue,
+        date: req.body.date,
+        installments: req.body.installments,
+        totalValue: req.body.totalValue,
+        description: req.body.description,
+        userClerkId: req.body.userClerkId,
       };
 
       const expenseCreated = await CreateExpense(expenseData);
