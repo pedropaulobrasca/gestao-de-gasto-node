9333e6111f91806fe48dfedca8b93c0b2f89f3fa
Pedro Brasca <pedropaulobrasca@gmail.com>
2023-11-13 19:07:46 -0300

    Add expense update route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get("/expense", ExpenseController.index);
 router.post("/expense", ExpenseController.create);
+router.put("/expense/:id", ExpenseController.update);
 router.delete("/expense/:id", ExpenseController.delete);
 
 export default router;
